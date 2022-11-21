496e3636a759d371c86475275ebc8347651ca340
Anna Pratnicka <pratnicka.ann@gmail.com>
2022-11-21 22:15:47 +0100

    finish the initAccordion function

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('new Product:', thisProduct);
     }
     renderInMenu() {
@@ -68,7 +69,18 @@
       menuContainer.appendChild(thisProduct.element);
 
     }
-
+    initAccordion() {
+      const thisProduct = this;
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      clickableTrigger.addEventListener('click', function (event) {
+        event.preventDefault();
+          const activeProduct = document.querySelector(select.all.menuProductsActive);
+          if (activeProduct && activeProduct !== thisProduct.element) {
+            activeProduct.classList.remove(classNames.menuProduct.wrapperActive);            
+          }
+          thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+      });
+    }
   }
 
   const app = {
@@ -95,6 +107,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+
     },
   };
 
