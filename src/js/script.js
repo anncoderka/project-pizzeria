222e58e011e06c22776f1e75799b7c534b1efe11
Anna Pratnicka <pratnicka.ann@gmail.com>
2022-12-01 09:00:43 +0100

    8.3 Cart basket

--- src/js/script.js
+++ src/js/script.js
@@ -31,6 +31,9 @@
         linkIncrease: 'a[href="#more"]',
       },
     },
+    cart: {
+      toggleTrigger: '.fa-chevron-down',
+    }
   };
 
   const classNames = {
@@ -38,6 +41,9 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    cart: {
+      wrapperActive: 'active',
+    }
   };
 
   const settings = {
@@ -177,37 +183,60 @@
       }
       thisWidget.input.value = thisWidget.value;
     }
-    initActions(){
+    initActions() {
       const thisWidget = this;
-      thisWidget.input.addEventListener('change', function(event){
+      thisWidget.input.addEventListener('change', function (event) {
         event.preventDefault();
         thisWidget.setValue(event.target.value);
       });
-      thisWidget.linkDecrease.addEventListener('click', function(event){
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
         event.preventDefault();
         thisWidget.setValue(thisWidget.value - 1);
       });
-      thisWidget.linkIncrease.addEventListener('click', function(event){
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
         event.preventDefault();
         thisWidget.setValue(thisWidget.value + 1);
       });
     }
   }
-
-
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+      console.log('*** initCart ***');
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+    }
+    getElements(element) {
+      const thisCart = this;
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+    initActions() {
+      const thisCart = this;
+      thisCart.dom.toggleTrigger.addEventListener('click', function () {
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
   const app = {
     initMenu: function () {
       console.log('*** initMenu ***');
       const thisApp = this;
-      console.log('thisApp.data', thisApp.data);
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
-
     },
     initData: function () {
       const thisApp = this;
       thisApp.data = dataSource;
+    },
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
     },
     init: function () {
       const thisApp = this;
@@ -219,7 +248,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
-
+      thisApp.initCart();
     },
   };
 
