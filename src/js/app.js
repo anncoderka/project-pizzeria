22af44e6bfa95a7cf1206d2564d6933708a435ec
Anna Pratnicka <pratnicka.ann@gmail.com>
2022-12-12 10:37:30 +0100

    10.4 Dodajemy Home page

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 import Booking from './components/Booking.js';
+import Home from './components/Home.js';
 
 const app = {
   initPages: function () {
@@ -71,11 +72,17 @@ const app = {
     const bookingElem = document.querySelector(select.containerOf.booking);
     thisApp.booking = new Booking(bookingElem);
   },
+  initHome() {
+    const thisApp = this;
+    const homeElem = document.querySelector(select.containerOf.home);
+    thisApp.booking = new Home(homeElem);
+  },
   init: function () {
     const thisApp = this;
     thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initHome();
     thisApp.initBooking();
   },
 };
