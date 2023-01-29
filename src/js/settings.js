7e32b97e882f53d3e9887f2d4a91c0ef54cec0f3
Anna Pratnicka <pratnicka.ann@gmail.com>
2023-01-29 14:27:03 +0100

    Reformat

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,78 +1,78 @@
 export const select = {
   templateOf: {
-    menuProduct: "#template-menu-product",
-    cartProduct: "#template-cart-product",
-    bookingWidget: "#template-booking-widget",
-    homeWidget: "#template-home-widget",
+    menuProduct: '#template-menu-product',
+    cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
+    homeWidget: '#template-home-widget',
   },
   containerOf: {
-    menu: "#product-list",
-    cart: "#cart",
-    pages: "#pages",
-    booking: ".booking-wrapper",
-    home: ".home-wrapper",
+    menu: '#product-list',
+    cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
+    home: '.home-wrapper',
   },
   all: {
-    menuProducts: "#product-list > .product",
-    menuProductsActive: "#product-list > .product.active",
-    formInputs: "input, select",
+    menuProducts: '#product-list > .product',
+    menuProductsActive: '#product-list > .product.active',
+    formInputs: 'input, select',
   },
   menuProduct: {
-    clickable: ".product__header",
-    form: ".product__order",
-    priceElem: ".product__total-price .price",
-    imageWrapper: ".product__images",
-    amountWidget: ".widget-amount",
+    clickable: '.product__header',
+    form: '.product__order',
+    priceElem: '.product__total-price .price',
+    imageWrapper: '.product__images',
+    amountWidget: '.widget-amount',
     cartButton: '[href="#add-to-cart"]',
   },
   widgets: {
     amount: {
-      input: "input.amount",
+      input: 'input.amount',
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
     datePicker: {
-      wrapper: ".date-picker",
-      input: `input[name="date"]`,
+      wrapper: '.date-picker',
+      input: `input[name='date']`,
     },
     hourPicker: {
-      wrapper: ".hour-picker",
+      wrapper: '.hour-picker',
       input: 'input[type="range"]',
-      output: ".output",
+      output: '.output',
     },
   },
   booking: {
-    peopleAmount: ".people-amount",
-    hoursAmount: ".hours-amount",
-    tables: ".floor-plan .table",
-    floorPlan: ".floor-plan",
-    form: "form",
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+    floorPlan: '.floor-plan',
+    form: 'form',
     phone: 'input[name="phone"]',
     address: 'input[name="address"]',
     starters: 'input[type="checkbox"]',
   },
   home: {
-    homeHeader: ".home-header",
+    homeHeader: '.home-header',
   },
   nav: {
-    links: ".main-nav a",
+    links: '.main-nav a',
   },
   cart: {
-    productList: ".cart__order-summary",
-    toggleTrigger: ".cart__summary",
+    productList: '.cart__order-summary',
+    toggleTrigger: '.cart__summary',
     totalNumber: `.cart__total-number`,
     totalPrice:
-      ".cart__total-price strong, .cart__order-total .cart__order-price-sum strong",
-    subtotalPrice: ".cart__order-subtotal .cart__order-price-sum strong",
-    deliveryFee: ".cart__order-delivery .cart__order-price-sum strong",
-    form: ".cart__order",
+      '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+    subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+    deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+    form: '.cart__order',
     formSubmit: '.cart__order [type="submit"]',
     phone: '[name="phone"]',
     address: '[name="address"]',
   },
   cartProduct: {
-    amountWidget: ".widget-amount",
-    price: ".cart__product-price",
+    amountWidget: '.widget-amount',
+    price: '.cart__product-price',
     edit: '[href="#edit"]',
     remove: '[href="#remove"]',
   },
@@ -80,21 +80,21 @@ export const select = {
 
 export const classNames = {
   menuProduct: {
-    wrapperActive: "active",
-    imageVisible: "active",
+    wrapperActive: 'active',
+    imageVisible: 'active',
   },
   cart: {
-    wrapperActive: "active",
+    wrapperActive: 'active',
   },
   booking: {
-    loading: "loading",
-    tableBooked: "booked",
+    loading: 'loading',
+    tableBooked: 'booked',
   },
   nav: {
-    active: "active",
+    active: 'active',
   },
   pages: {
-    active: "active",
+    active: 'active',
   },
 };
 
@@ -108,21 +108,21 @@ export const settings = {
     defaultDeliveryFee: 20,
   },
   db: {
-    products: "products",
-    orders: "orders",
+    products: 'products',
+    orders: 'orders',
 
     url:
-      "//" +
+      '//' +
       window.location.hostname +
-      (window.location.hostname == "localhost" ? ":3131" : ""),
-    product: "product",
-    order: "order",
-    booking: "bookings",
-    event: "events",
-    dateStartParamKey: "date_gte",
-    dateEndParamKey: "date_lte",
-    notRepeatParam: "repeat=false",
-    repeatParam: "repeat_ne=false",
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
+    product: 'product',
+    order: 'order',
+    booking: 'bookings',
+    event: 'events',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
   },
   hours: {
     open: 12,
@@ -132,7 +132,7 @@ export const settings = {
     maxDaysInFuture: 14,
   },
   booking: {
-    tableIdAttribute: "data-table",
+    tableIdAttribute: 'data-table',
   },
 };
 
